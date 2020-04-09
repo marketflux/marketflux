898cd20c32ea224c525062022a821ee89cdf7f64
marketflux <marketflux.hotmail.com>
2020-04-09 10:20:57 +0530

    09apr20

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+require('./gulp/task/styles');
+require('./gulp/task/watch');
+require('./gulp/task/sprites');
+require('./gulp/task/scripts');
+//require('./gulp/tasks/modernizr');
+//require('./gulp/tasks/build');
